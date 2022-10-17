7e86ecc4cb78aff48f3f8a68924031cb2945b210
lambtron <andyjiang@gmail.com>
2022-10-17 07:39:40 -0700

    first commit

---
+++ my-vue-app/src/components/Dinosaur.vue
@@ -0,0 +1,22 @@
+<script>
+import { ref } from 'vue'
+
+export default {
+  // props: [ 'name', 'description' ]
+  props: {
+    description: {
+      type: String,
+      required: true
+    }
+  },
+  inheritAttrs:false
+
+}
+</script>
+
+
+<template>
+  {{ this.$route.params.dinosaur }}
+  {{ this.$route.params.description }}
+
+</template>
